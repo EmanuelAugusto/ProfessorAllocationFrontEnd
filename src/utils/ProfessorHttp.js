1a075e6c4ea27a6d743e3e4aa0548e1ec2286dd5
EmanuelAugusto <emanuel02augusto@gmail.com>
2022-07-09 13:13:46 -0300

    adjust

--- src/utils/ProfessorHttp.js
+++ src/utils/ProfessorHttp.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const URL_PROD = null;
+const URL_PROD = "https://backallocation.herokuapp.com/";
 
 const URL_LOCALHOST = "http://localhost:8080/";
 
 const ProfessorHttp = axios.create({
-    baseURL: URL_LOCALHOST
+    baseURL: URL_PROD
 })
 
 
